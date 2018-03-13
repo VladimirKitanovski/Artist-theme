6308493c68186e4e55cfc6dcd37b7abc1e50bc44
Vladimir Kitanovski <36275048+VladimirKitanovski@users.noreply.github.com>
2018-03-13 18:00:44 +0100

    Add files via upload

---
+++ _site/assets/js/functions.js
@@ -0,0 +1,106 @@
+$(function() {
+	smoothScroll(1000);
+	workBelt();
+	workLoad();
+	typeAnimation();
+
+	$("header h1").fitText(1, { minFontSize: '20px', maxFontSize: '72px' });
+});
+
+// smoothScrool function applied
+function smoothScroll (duration) {
+	$('a[href^="#"]').on('click', function(event) {
+
+		var target = $( $(this).attr('href') );
+
+		if (target.length) {
+			event.preventDefault();
+			$('html, body').animate({
+				scrollTop: target.offset().top
+			}, duration);
+
+		}
+	});
+}
+
+function workBelt() {
+
+  $('.thumb-unit').click(function() {
+    $('.work-belt').css("left","-100%");
+    $('.work-container').show();
+  });
+
+  $('.work-return').click(function() {
+    $('.work-belt').css("left","0%");
+    $('.work-container').hide(800);
+  });
+
+}
+
+function workLoad() {
+
+	$.ajaxSetup({ cache: true });
+
+	$('.thumb-unit').click(function() {
+
+		var $this = $(this),
+			newTitle = $this.find('strong').text(),
+			newfolder = $this.data('folder'),
+			spinner = '<div class="loader">Loading...</div>',
+			newHTML = '/work/'+ newfolder +'.html';
+		$('.project-load').html(spinner).load(newHTML);
+		$('.project-title').text(newTitle);
+	});
+
+}
+	function typeAnimation() {
+		Typed.new("#writing-text", {
+			strings: [
+				"am a Full-Stack Web Developer.", "love everything about code.", "also teach programming to people.", "solve problems."
+			],
+			// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+			stringsElement: null,
+			// typing speed
+			typeSpeed: 1,
+			contentType: 'text',
+			callback: function() {
+				$("#writing-text").css({"color": "#fff", "background-color": "#C8412B"});
+			},
+			preStringTyped: function() {},
+			onStringTyped: function() {}
+		});
+	}
+
+
+(function( $ ){
+
+  $.fn.fitText = function( kompressor, options ) {
+
+    // Setup options
+    var compressor = kompressor || 1,
+        settings = $.extend({
+          'minFontSize' : Number.NEGATIVE_INFINITY,
+          'maxFontSize' : Number.POSITIVE_INFINITY
+        }, options);
+
+    return this.each(function(){
+
+      // Store the object
+      var $this = $(this);
+
+      // Resizer() resizes items based on the object width divided by the compressor * 10
+      var resizer = function () {
+        $this.css('font-size', Math.max(Math.min($this.width() / (compressor*10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)));
+      };
+
+      // Call once to set.
+      resizer();
+
+      // Call on resize. Opera debounces their resize by default.
+      $(window).on('resize.fittext orientationchange.fittext', resizer);
+
+    });
+
+  };
+
+})( jQuery );
